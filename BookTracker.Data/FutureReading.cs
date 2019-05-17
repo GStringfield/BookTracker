ee13052385ad95b23d763215af33d239482554a2
G S <gstringfield@live.com>
2019-05-17 10:19:19 -0400

    Navbar fixed

---
+++ BookTracker.Data/FutureReading.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTracker.Data
+{
+   public  class FutureReading
+    {
+        [Key]
+        public int FutureReadingID { get; set; }
+        //foriegn key
+        public int BookID { get; set; }
+
+        [Required]
+        public Guid UserID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        //this is my navigation propery
+        public virtual Book Book
+        { get; set; }
+
+    }
+}
